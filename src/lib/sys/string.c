5164b2f99aa71c4c95dd952af3f906b8fa1bdb2c
kimizdevv <kimizx7@gmail.com>
2023-12-30 11:11:45 +0100

    Expand the string library

--- src/lib/sys/string.c
+++ src/lib/sys/string.c
@@ -2,12 +2,30 @@
 
 _Bool strequ(const char *s1, const char *s2)
 {
-        while (*s1 && (*s1 == *s2)) {
-                s1++;
-                s2++;
-        }
+        while (*s1 && (*s1 == *s2))
+                s1++, s2++;
 
         return *(unsigned char *)s1 == *(unsigned char *)s2;
+}
+
+static char let_upr(const char let)
+{
+        return (char)((let >= 'a' && let <= 'z') ? let - 32 : let);
+}
+_Bool strequ_upr(const char *s1, const char *s2)
+{
+        while (*s1 && (let_upr(*s1) == let_upr(*s2)))
+                s1++, s2++;
+
+        return *(unsigned char *)s1 == *(unsigned char *)s2;
+}
+
+void strtoupr(char *s)
+{
+        while (*s) {
+                *s = let_upr(*s);
+                *s++;
+        }
 }
 
 size_t strlen(const char *s)
@@ -34,4 +52,36 @@ void strclr(char *s)
         do {
                 *s++ = 0;
         } while (*s != 0);
+}
+
+int stoi(const char *s, int *status)
+{
+        int res = 0;
+        *status = 0;
+        _Bool neg = 0;
+        _Bool found_num = 0;
+
+        while (*s) {
+                const char c = *s++;
+
+                if (!found_num && c == '-') {
+                        neg = !neg;
+                        continue;
+                }
+
+                if (c < '0' || c > '9') {
+                        *status = -1;
+                        break;
+                }
+
+                found_num = 1;
+
+                const int d = c - '0';
+                res = res * 10 + d;
+        }
+
+        if (!found_num)
+                *status = -2;
+
+        return neg ? -res : res;
 }
--- src/lib/sys/string.h
+++ src/lib/sys/string.h
@@ -4,11 +4,16 @@
 #include <stddef.h>
 
 _Bool strequ(const char *s1, const char *s2);
+_Bool strequ_upr(const char *s1, const char *s2);
+
+void strtoupr(char *s);
 
 size_t strlen(const char *s);
 
 char *strcpy(char *dst, const char *src);
 
 void strclr(char *s);
+
+int stoi(const char *s, int *status);
 
 #endif
